a7e062dc176cf0137c0075b6c8972dbe0cc1a23f
四不和声 <1223736337@qq.com>
2026-01-08 09:50:19 +0800

    dev：简化item的buff效果列表配置规则

--- Assets/_Scripts/RefObj/EffectObj/BuffEffectObj.cs
+++ Assets/_Scripts/RefObj/EffectObj/BuffEffectObj.cs
@@ -6,16 +6,14 @@ namespace GameCore.RefData
     public class BuffEffectObj : _AEffectObjBase
     {
         public long buffRefObjId;
-        public int buffChgLevel;
         public int continueTurn;
         protected override void OnDeserialize(string _str)
         {
             string[] strArr = _str.Split(":");
-            if (strArr == null || strArr.Length < 3)
+            if (strArr == null || strArr.Length < 2)
                 return;
             buffRefObjId = SCCommon.ParseLong(strArr[0]);
-            buffChgLevel = SCCommon.ParseInt(strArr[1]);
-            continueTurn = SCCommon.ParseInt(strArr[2]);
+            continueTurn = SCCommon.ParseInt(strArr[1]);
         }
 
         protected override string OnSerialise()
